8e6ef83449e8d19e9561d7a1a7d8d563f3ef8a71
Yan <yans@yancomm.net>
2016-07-06 19:57:17 -0700

    be more flexible in path discovery

--- shellphish_qemu/__init__.py
+++ shellphish_qemu/__init__.py
@@ -2,10 +2,19 @@ import os
 import distutils
 
 def qemu_path(platform):
-    basename = 'shellphish-qemu-%s' % platform
-    if __file__.startswith(distutils.sysconfig.PREFIX):
-        path = os.path.join(distutils.sysconfig.PREFIX, 'bin', basename)
-    else:
-        path = os.path.join(os.path.dirname(__file__), '..', 'bin', basename)
+    for basename in (
+        'shellphish-qemu-%s' % platform,
+        'shellphish-qemu-linux-%s' % platform,
+        '%s' % platform,
+    ):
+        path = os.path.join(qemu_base(), basename)
+        if os.path.exists(path):
+            return path
 
-    return path if os.path.exists(path) else None
+    return "NOT_FOUND"
+
+def qemu_base():
+    if __file__.startswith(distutils.sysconfig.PREFIX):
+        return os.path.join(distutils.sysconfig.PREFIX, 'bin')
+    else:
+        return os.path.join(os.path.dirname(__file__), '..', 'bin')
