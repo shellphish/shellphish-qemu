bcf28dc27d790721cd1b2201eaa00a739073c40c
Andrew Dutcher <andrew@andrewdutcher.com>
2017-01-05 07:59:21 -0800

    Fix embarassing error, add qemu_list function

--- shellphish_qemu/__init__.py
+++ shellphish_qemu/__init__.py
@@ -1,4 +1,7 @@
 import os
+import pkg_resources
+
+__all__ = ('qemu_path', 'qemu_base', 'qemu_list')
 
 def qemu_path(platform):
     for basename in (
@@ -14,3 +17,9 @@ def qemu_path(platform):
 
 def qemu_base():
     return pkg_resources.resource_filename('shellphish_qemu', 'bin')
+
+def qemu_list():
+    qdir = os.listdir(qemu_base())
+    prefix = 'shellphish-qemu-'
+    pl = len(prefix)
+    return sorted(x[pl:] for x in qdir if x.startswith(prefix))
