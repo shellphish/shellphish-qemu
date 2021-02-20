f72673bd67ed5a2c81ce7aeba12266eb57b89d25
Kevin Phoenix <kevin@kphoenix.us>
2021-02-20 13:30:40 -0700

    Update version to 0.11.0

--- build_wheels.sh
+++ build_wheels.sh
@@ -7,10 +7,15 @@ docker build -t shellphish_qemu_build_image -f - $checkout_dir << EOF
 FROM quay.io/pypa/manylinux_2_24_x86_64
 
 RUN sed -i -E "s/^deb (.+)$/deb \\1\\ndeb-src \\1\\n/g" /etc/apt/sources.list
-RUN apt-get update; apt-get build-dep -y qemu
+RUN apt-get update; apt-get build-dep -y qemu; apt-get install ninja-build
+
+RUN useradd build
+USER build
+
+ENV PATH=/opt/python/cp36-cp36m/bin:$PATH
 
 WORKDIR /build
-CMD /opt/python/cp36-cp36m/bin/python setup.py bdist_wheel --plat-name manylinux_2_24_x86_64 && \
+CMD python3 setup.py bdist_wheel --plat-name manylinux_2_24_x86_64 && \
     auditwheel repair --plat manylinux_2_24_x86_64 dist/*.whl
 EOF
 
