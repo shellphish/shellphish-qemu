6ed5e32218cff3072cb2192cf9cb547775a1e8c7
Kyle ZENG <jkjh1jkjh1@gmail.com>
2021-04-29 16:50:59 -0700

    build wheels in ubuntu to avoid building weird buggy qemu

--- build_wheels.sh
+++ build_wheels.sh
@@ -4,10 +4,14 @@ set -ex
 checkout_dir=$(realpath $(dirname $0))
 
 docker build -t shellphish_qemu_build_image -f - $checkout_dir << EOF
-FROM quay.io/pypa/manylinux_2_24_x86_64
+FROM ubuntu:focal
+
+ENV DEBIAN_FRONTEND="noninteractive"
 
 RUN sed -i -E "s/^deb (.+)$/deb \\1\\ndeb-src \\1\\n/g" /etc/apt/sources.list
 RUN apt-get update; apt-get build-dep -y qemu; apt-get install ninja-build
+RUN apt-get install -y python3-setuptools python3-pip python2
+RUN pip3 install -y wheel auditwheel
 
 RUN useradd build
 USER build
