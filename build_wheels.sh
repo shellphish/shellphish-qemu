989d90dcf2d15f9c909a69f281433b6b544bebc8
Kyle Zeng <Kyle-Kyle@users.noreply.github.com>
2021-04-29 20:53:03 -0700

    Build wheels in ubuntu to avoid building weird buggy qemu (#17)
    * build wheels in ubuntu to avoid building weird buggy qemu
    
    * Fix missing deps in build_wheels.sh
    
    * Add comment about base image
    
    Co-authored-by: Kevin Phoenix <kevin@kphoenix.us>

--- build_wheels.sh
+++ build_wheels.sh
@@ -4,15 +4,17 @@ set -ex
 checkout_dir=$(realpath $(dirname $0))
 
 docker build -t shellphish_qemu_build_image -f - $checkout_dir << EOF
-FROM quay.io/pypa/manylinux_2_24_x86_64
+FROM ubuntu:focal # This is OK because all binaries are static
+
+ENV DEBIAN_FRONTEND="noninteractive"
 
 RUN sed -i -E "s/^deb (.+)$/deb \\1\\ndeb-src \\1\\n/g" /etc/apt/sources.list
-RUN apt-get update; apt-get build-dep -y qemu; apt-get install ninja-build
+RUN apt-get update; apt-get build-dep -y qemu
+RUN apt-get install -y python3-setuptools python3-pip python2 ninja-build unzip patchelf
+RUN pip3 install wheel auditwheel
 
 RUN useradd build
 USER build
-
-ENV PATH=/opt/python/cp36-cp36m/bin:$PATH
 
 WORKDIR /build
 CMD python3 setup.py bdist_wheel --plat-name manylinux_2_24_x86_64 && \
