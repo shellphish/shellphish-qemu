67f083f45e8b57df75940d451af8b7fd858b2786
Kevin Phoenix <kevin@kphoenix.us>
2021-05-10 23:21:45 +0000

    Update build_wheels.sh script to use manylinux2010 platform tag

--- build_wheels.sh
+++ build_wheels.sh
@@ -11,15 +11,15 @@ ENV DEBIAN_FRONTEND="noninteractive"
 
 RUN sed -i -E "s/^deb (.+)$/deb \\1\\ndeb-src \\1\\n/g" /etc/apt/sources.list
 RUN apt-get update; apt-get build-dep -y qemu
-RUN apt-get install -y python3-setuptools python3-pip python2 ninja-build unzip patchelf
+RUN apt-get install -y python3-setuptools python3-pip python2 ninja-build unzip patchelf git
 RUN pip3 install wheel auditwheel
 
 RUN useradd build
 USER build
 
 WORKDIR /build
-CMD python3 setup.py bdist_wheel --plat-name manylinux_2_24_x86_64 && \
-    auditwheel repair --plat manylinux_2_24_x86_64 dist/*.whl
+CMD python3 setup.py bdist_wheel --plat-name manylinux2010_x86_64 && \
+    auditwheel repair --plat manylinux2010_x86_64 dist/*.whl
 EOF
 
 docker run --rm --name shellphish_qemu_build -v $checkout_dir:/build -t shellphish_qemu_build_image
